11aebc9092411d37ac0b0ea2c1aab9482bdbd733
Linyugui <446046940@qq.com>
2019-10-31 16:01:33 +0800

    init

---
+++ StaticFramework.framework/Headers/StaticFramework.h
@@ -0,0 +1,21 @@
+//
+//  StaticFramework.h
+//  StaticFramework
+//
+//  Created by squarelin on 2019/10/29.
+//  Copyright © 2019年 squarelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StaticFramework.
+FOUNDATION_EXPORT double StaticFrameworkVersionNumber;
+
+//! Project version string for StaticFramework.
+FOUNDATION_EXPORT const unsigned char StaticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StaticFramework/PublicHeader.h>
+
+
+#import <StaticFramework/StaticTest.h>
+
---
+++ StaticFramework.framework/Headers/StaticTest.h
@@ -0,0 +1,18 @@
+//
+//  StaticTest.h
+//  StaticFramework
+//
+//  Created by squarelin on 2019/10/29.
+//  Copyright © 2019年 squarelin. All rights reserved.
+//
+
+#ifndef StaticTest_h
+#define StaticTest_h
+
+@interface StaticTest : NSObject
+
+-(void)sayHello;
+
+@end
+
+#endif /* StaticTest_h */
